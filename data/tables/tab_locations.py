c334d0267f81a72bb203acbc828b7e9933eedf3f
venbruno <bruno.dalle@yahoo.com.br>
2023-01-02 11:42:44 -0300

    Build order_items table plus minor adjustments.

--- data/tables/tab_locations.py
+++ data/tables/tab_locations.py
@@ -1,6 +1,7 @@
 from faker import Faker
 import pandas as pd
 from geopy.geocoders import Nominatim
+import random
 
 geocoder = Nominatim(user_agent = 'your_app_name')
 
@@ -9,6 +10,7 @@ fake = Faker('pt_BR')
 ids = []
 cities = []
 states = []
+city_tax = []
 
 for row in range(0, 50+1):
     
@@ -24,13 +26,16 @@ for row in range(0, 50+1):
         states.append('Not Found')
     else:
         states.append(loc.get('state'))
+    
+    city_tax.append(round(random.uniform(0.05, 0.15), 4))
 
 location = {
     'id': ids,
     'city': cities,
-    'state': states
+    'state': states,
+    'city_tax': city_tax
 }
 
-df = pd.DataFrame(location, columns = ['id', 'city', 'state'])
+df = pd.DataFrame(location, columns = ['id', 'city', 'state', 'city_tax'])
 path = '../csvs/locations.csv'
 df.to_csv(path_or_buf=path, encoding='utf-8', index=False)
--- data/tables/tab_products.py
+++ data/tables/tab_products.py
@@ -10,91 +10,106 @@ pet_shop_list = [
         "category":"Dog",
         "subcategory":"Toy",
         "title":"Power Chew Double Bone",
-        "detail":"XL - $30.69",
+        "size":"XL",
+        "price":"30.69"
     },
     {
         "category":"Dog",
         "subcategory":"Toy",
         "title":"CoreStrength Rattlez Ball",
-        "detail":"L - $9.99",
+        "size":"L",
+        "price":"9.99"
     },
     {
         "category":"Dog",
         "subcategory":"Toy",
         "title":"Bacon Flavor Wishbone Tough",
-        "detail":"M - $8.99",
+        "size":"M",
+        "price":"8.99"
     },
     {
         "category":"Dog",
         "subcategory":"Toy",
         "title":"Extreme Tires",
-        "detail":"L - $15.99",
+        "size":"L",
+        "price":"15.99"
     },
     {
         "category":"Dog",
         "subcategory":"Toy",
         "title":"Lobster",
-        "detail":"M - $10.59",
+        "size":"M",
+        "price":"10.59"
     },
     {
         "category":"Dog",
         "subcategory":"Treats",
         "title":"Peanut Butter Oven Baked",
-        "detail":"M - $4.07",
+        "size":"M",
+        "price":"4.07"
     },
     {
         "category":"Dog",
         "subcategory":"Treats",
         "title":"Crunchy with Real Mixed Berries",
-        "detail":"S - $8.98",
+        "size":"S",
+        "price":"8.98"
     },
     {
         "category":"Dog",
         "subcategory":"Treats",
         "title":"Classic Chicken",
-        "detail":"S - $6.37",
+        "size":"S",
+        "price":"6.37"
     },
     {
         "category":"Cat",
         "subcategory":"Toy",
         "title":"Colorful Springs",
-        "detail":"10 Count - $4.75",
+        "size":"10 Count",
+        "price":"4.75"
     },
     {
         "category":"Cat",
         "subcategory":"Toy",
         "title":"Basic Plush Mice",
-        "detail":"5 Count - $4.64",
+        "size":"5 Count",
+        "price":"4.64"
     },
     {
         "category":"Cat",
         "subcategory":"Toy",
         "title":"Guacamole Bowl",
-        "detail":"4 Count - $5.64",
+        "size":"4 Count",
+        "price":"5.64"
     },
     {
         "category":"Cat",
         "subcategory":"Toy",
         "title":"Kitty Crinkle Balls Indoor",
-        "detail":"12 Count - $6.33",
+        "size":"12 Count",
+        "price":"6.33"
     },
     {
         "category":"Cat",
         "subcategory":"Treat",
         "title":"Party Mix Beachside Crunch Flavor",
-        "detail":"20-oz - $10.29",
+        "size":"20-oz",
+        "price":"10.29"
     },
     {
         "category":"Cat",
         "subcategory":"Treat",
         "title":"Irresistibles Soft Salmon",
-        "detail":"3-oz - $1.49",
+        "size":"3-oz",
+        "price":"1.49"
     },
     {
         "category":"Cat",
         "subcategory":"Treat",
         "title":"MixUps Catnip Fever Flavor",
-        "detail":"16-oz - $8.49",
+        "size":"16-oz",
+        "price":"8.49"
     }
 ]
 
@@ -102,7 +117,8 @@ id = []
 title = []
 category = []
 subcategory = []
-detail = []
+size = []
+price = []
 
 ind = 0
 for item in pet_shop_list:
@@ -116,16 +132,19 @@ for item in pet_shop_list:
 
     subcategory.append(item['subcategory'])
 
-    detail.append(item['detail'])
+    size.append(item['size'])
+    
+    price.append(item['price'])
 
 product = {
     'id': id,
     'title': title,
     'category': category,
     'subcategory': subcategory,
-    'detail': detail,
+    'size': size,
+    'price': price
 }
 
-df = pd.DataFrame(product, columns = ['id', 'title', 'category', 'subcategory', 'detail'])
+df = pd.DataFrame(product, columns = ['id', 'title', 'category', 'subcategory', 'size', 'price'])
 path = '../csvs/products.csv'
 df.to_csv(path_or_buf=path, encoding='utf-8', index=False)
